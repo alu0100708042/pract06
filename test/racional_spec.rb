e9f85deff20206e5595885c0afce83a8e31ca2be
Oliver Martínez Novo <alu0100704129@ull.edu.es>
2013-10-24 19:39:35 +0100

    Test para comprobar numerador y denominador

--- test/racional_spec.rb
+++ test/racional_spec.rb
@@ -5,8 +5,16 @@ define Racional do
 	
 	// Se ejecuta antes de cada prueba.
 	before :each do
-		@racional1 = Racional.new(1,5)		
-		@racional2 = Racional.new(1,3)		
+		@racionalA = Racional.new(1,5)		
+		@racionalB = Racional.new(1,3)		
+	end
+	
+	it "Comprobar si existe numerador" do
+		@racionalA.num.should == 1
+	end
+
+	it "Comprobar si existe denominador" do
+		@racionalA.denom.should == 5
 	end
 	
 
