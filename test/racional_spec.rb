95038f557eb2fca44f560b073fb62996ad953432
Oliver Martínez Novo <alu0100704129@ull.edu.es>
2013-10-24 19:28:49 +0100

    Declaración de variables para todas las pruebas

--- test/racional_spec.rb
+++ test/racional_spec.rb
@@ -1,4 +1,13 @@
 require "rspec"
 require "racional"
 
+define Racional do
+	
+	// Se ejecuta antes de cada prueba.
+	before :each do
+		@racional1 = Racional.new(1,5)		
+		@racional2 = Racional.new(1,3)		
+	end
+	
 
+end
