a9f44c78b8e0141a43569f0fa39f8fa3c7d39b7f
Oliver Martinez <alu0100704129@ull.edu.es>
2013-10-25 11:16:33 +0100

    Test para la funcion de punto flotante

--- test/racional_spec.rb
+++ test/racional_spec.rb
@@ -30,17 +30,12 @@ describe Racional do
 	end
 	
 	it "Comprobar que este en forma reducida" do
-		@racional.should == Racional.new(1,3)
+		@racionalB.to_s.should == Racional.new(1,3).to_s
 	end
 	
-	
-
-
-
-
-
-
-
+	it "Comprobar el formato flotante" do
+		@racionalA.to_f.should == 1.to_f/5.to_f
+	end
 
 	
 end
